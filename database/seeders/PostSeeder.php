65992d20f4fa8ca416d8811cbeea28eab18f31b8
Manuel della Gala <manuel.dellagala@gmail.com>
2023-05-17 10:51:11 +0200

    init

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('posts')->insert([
+            'title' => 'Prova post 1',
+            'description' => 'Test primo post',
+            'photo' => 'NULL'
+        ]);
+    }
+}
